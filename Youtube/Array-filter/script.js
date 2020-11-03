f70135a2d0f7642844bee8949abed4a69f6dd4e7
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-11-03 10:51:15 +0200

    array filter

--- Youtube/Array-filter/script.js
+++ Youtube/Array-filter/script.js
@@ -1,1 +1,15 @@
 // TASK:Create a function that accepts an array as an argument. It is necessary to print all digits above zero, remove letters and negative digits, sort the array.
+let size = ["10px", "3em", "35pt", "-25px", "50%"];
+
+function calc(arr) {
+let out = [];
+    for (let i=0; i<arr.length; i++) {
+        arr[i] = parseFloat(arr[i]);
+        if (arr[i] >= 0) out.push(arr[i]);
+    }
+    out.sort(function (a, b) {
+        return a-b;
+    });
+    return out;
+}
+console.log( calc(size) );
