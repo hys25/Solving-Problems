f88a1282e7a301642b82925c52d6a7d46580952c
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-11-03 15:43:44 +0200

    schedules on Chart.js

---
+++ Youtube/Chart.js-library/script.js
@@ -0,0 +1,14 @@
+let ctx = document.getElementById('myChart').getContext('2d');
+let chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My chart',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+    options: {}
+});
