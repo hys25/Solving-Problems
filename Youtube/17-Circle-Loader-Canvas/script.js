456e04da6eb42dfc515582acede89cd9c8228867
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2020-11-26 11:45:28 +0200

    Easy interviewing tasks

---
+++ Youtube/17-Circle-Loader-Canvas/script.js
@@ -0,0 +1,44 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+let pi = Math.PI;
+let k = 2 * pi / 100;
+let x = 0;
+let max = 100;
+let timeout;
+
+draw();
+
+function draw(){
+    let color = getColor(x);
+    ctx.clearRect(0, 0, 250, 250);
+
+    ctx.beginPath();
+    ctx.arc(125, 125, 100, 0, x*k, false);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 20;
+    ctx.lineCap= 'round';
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.font = 'bold 70px Arial';
+    ctx.fillStyle = color;
+    ctx.textAlign = 'center';
+    ctx.fillText(x + '%', 125, 145);
+    ctx.closePath();
+
+    x++;
+    if(x <= max){
+        timeout = setTimeout(draw, 50);
+    } else {
+        clearTimeout(timeout);
+    }
+}
+
+function getColor(x){
+    x = x * 255 / 100;
+    y = 255 - x;
+    const color = 'rgb('+x+', '+y+', 0)';
+    return color;
+}
